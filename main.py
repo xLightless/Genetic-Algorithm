9097498dcd828bfa3982accf3b026d6cd33eeb44
xLightless <lightlessgaming@gmail.com>
2023-10-24 06:53:03 +0100

    added fitness best and mean

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ import numpy as np
 
 ## Initial Population
 N = 10 # N is the number of genome in a chromosome of an Individual. It will also be used as a random.randint(1, N) for a crossover point.
-P = 4 # P is the size of the population.
-MUTRATE = 4 # Mutation rate of the ?
+P = 50 # P is the size of the population.
+MUTRATE = 0.5 # Mutation rate of the ? 0.001
+
+# Record the best fitness of an individual from a population
+best_fitness = None
+mean_fitness = []
 
 class Individual:    
     def __init__(
@@ -18,26 +22,18 @@ class Individual:
     ):
         self.gene = [0]*N
         self.fitness = 0
-
-
-population = []
+        
+print("\n**** Genetic Algorithm Summary ****")
         
 # INITIALISE population with random candidate solutions
+population = []
 for _ in range(0, P):
     temp_gene = [random.randint(0, 1) for j in range(0, N)]
     new_individual = Individual()
     new_individual.gene = temp_gene.copy()
     population.append(new_individual)
 
-## EVALUATE each candidate
-def fitness(individual):
-    """ Fitness function - loop through genome to get the total fitness. """
-    
-    ## Count all the binary 1s in a chromosome. 
-    utility = 0
-    for i in range(N):
-        utility = utility + individual.gene[i]
-    return utility
+print("Intialised population size: %s... \nWith chromosome length: %s" % (len(population), N))
 
 ## Tournament Selection
 offspring = []  
@@ -51,6 +47,21 @@ for i in range(0, P):
         offspring.append(offspring_one)
     else:
         offspring.append(offspring_two)
+        
+## EVALUATE each candidate
+def fitness(individual):
+    """ Fitness function - loop through genome to get the total fitness. """
+    
+    ## Count all the binary 1s in a chromosome. 
+    utility = 0
+    for i in range(N):
+        utility = utility + individual.gene[i]
+    return utility
+        
+# # Set population[i] to each iterative fitness total.
+# for i in range(P):
+#     ## Set the fitness for each individual in population
+#     population[i] = fitness(population[i])
         
 ## Crossover
 toff1 = Individual() 
@@ -69,8 +80,10 @@ for i in range(0, P, 2):
     offspring[i] = copy.deepcopy(toff1)
     offspring[i+1] = copy.deepcopy(toff2)
 
-
 ## Mutation
+population.clear()
+print("\nIntialised population being mutated...")
+
 for i in range(0, P): 
     newind = Individual(); 
     newind.gene = [] 
@@ -81,20 +94,62 @@ for i in range(0, P):
             if (gene == 1): 
                 gene = 0 
             else: 
-                gene = 1 
-        newind.gene.append(gene) 
-
-## Set population[i] to each iterative fitness total.
-for i in range(P):
-    ## Set the fitness for each individual in population
-    population[i] = fitness(population[i])
+                gene = 1
+                
+        ## Append genome into a chromosome set of genes
+        newind.gene.append(gene)
+        
+    ## You must then append new individual or overwrite offspring
+    population.append(newind)
     
-np_population = np.array(population)
+    ## Set the fitness for each offspring in population
+    newind.fitness = fitness(newind)
+    
+    
+    if best_fitness is None:
+        best_fitness = newind.fitness
+        
+    if newind.fitness > best_fitness:
+        best_fitness = newind.fitness
+        
+    if mean_fitness is None:
+        mean_fitness = newind.fitness
+        
+    else:
+        mean_fitness.append(newind.fitness)
 
-plt.plot(np_population)
-plt.ylabel("Population Fitness")
-plt.xlabel("Individual Positional Number")
+    population[i] = fitness(population[i])
+
+## Get best fitness and mean fitness
+mean_fitness = np.mean(mean_fitness)
+best_fitness = best_fitness
+
+
+
+
+
+
+
+
+print("Best Fitness: %s \nMean Fitness: %s" % (best_fitness, mean_fitness))
+
+
+
+# print(population)
+plt.plot(population)
+plt.xlabel("Generation")
+plt.ylabel("Fitness")
 plt.show()
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -113,3 +168,6 @@ plt.show()
 # (4) Replace: Use newly generated population for a further run of algorithm.					
 # (5) Test: If the end condition is satisfied, stop, and return the best solution in current population					
 # (6) Loop: Go to step 2
+
+# https://www.researchgate.net/post/How-can-one-get-the-average-and-best-fitness-in-genetic-algorithm
+# https://softwareengineering.stackexchange.com/questions/177474/matlab-best-fitness-vs-mean-fitness-initial-range
